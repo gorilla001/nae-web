376cfc9c77380989eb3ee723cad1e32e6d0af404
niuminguo <none@none>
2014-11-11 15:58:17 +0800

    修改auth_login broker_pipe bug
    --HG--
    branch : minguon-dev

--- auth/views.py
+++ auth/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render_to_response,HttpResponseRedirect
 from django.template import RequestContext
-#from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_protect
 from django.core.context_processors import csrf
 import requests
@@ -11,6 +10,7 @@ from django.contrib.auth import authenticate, login
 import hashlib, time
 
 # Create your views here.
+
 def auth_login(request):
     auth_username = request.GET['username']
     request.session['is_authed'] = True;
@@ -38,23 +38,5 @@ def auth_login(request):
     for item in auth_result.json():
         if item['uid'] == auth_username :
             request.session['user_key'] = item['key']
-    #url= http://auth.jumeird.com/api/allkey/?app_key=1adcdec244fb11e28dbe7cd1c3eeb629&app_name=optool_jumei
     request.session.set_expiry(6000)
     return HttpResponseRedirect('/')
-    #if len(auth_data["groups"]) > 0:
-    #content["Role"] = auth_data["groups"]
-    #memberurl = "%s%s%s%s%s" % (auth_url, "api/member/?uid=", auth_username, app_key, app_name)
-    #headers = {'content-type': 'application/json'}
-    #member_result = requests.get(memberurl, headers=headers,)
-    #memberdata = member_result.json()
-    #user_list = User.objects.get(username=auth_username)
-    #user_list.backend = 'django.contrib.auth.backends.ModelBackend'
-    ## login(request,user_list)
-    #session_id = hashlib.sha1(auth_token_id + "481986a634ca11e4ab8c842b2b738d12" + auth_username).hexdigest()
-    #user.backend = 'django.contrib.auth.backends.ModelBackend'
-    #login(request,user)
-    #request.session.set_expiry(60 * 1 )
-    #request.session["session_id"] = "asdfasdfasdfadsf89089089sadfasd342340"
-    #content.update(csrf(request))
-    ## else:
-    #     return render_to_response('default/error.html')
