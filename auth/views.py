630a0cedf2d7734d9fdcdbd92c78644efdfdc45c
niuminguo <none@none>
2014-11-11 17:05:24 +0800

    修改auth_login broker_pipe bug
    --HG--
    branch : minguon-dev

--- auth/views.py
+++ auth/views.py
@@ -16,34 +16,31 @@ signal(SIGPIPE,SIG_DFL)
 # Create your views here.
 
 def auth_login(request):
-    try:
-        auth_username = request.GET['username']
-        request.session['is_authed'] = True;
-        url = "{}{}{}{}{}{}".format(auth_url,"/api/member/?uid=",auth_username,app_key,auth_key,app_name)
-        headers={'Content-Type':'application/json'}
-        auth_result = requests.get(url, headers=headers)
-        resp_data = auth_result.json()
-        if 'success' in resp_data:
-            user_id = resp_data['username'] 
-            user_name = resp_data['fullname']
+    auth_username = request.GET['username']
+    request.session['is_authed'] = True;
+    url = "{}{}{}{}{}{}".format(auth_url,"/api/member/?uid=",auth_username,app_key,auth_key,app_name)
+    headers={'Content-Type':'application/json'}
+    auth_result = requests.get(url, headers=headers)
+    resp_data = auth_result.json()
+    if 'success' in resp_data:
+        user_id = resp_data['username'] 
+        user_name = resp_data['fullname']
 
-            request.session['user_id'] = user_id 
-            request.session['user_name'] = user_name 
-        url = "%s%s%s%s%s%s" % (auth_url, "/api/grouprole/?uid=", auth_username, app_key, auth_key, app_name)
-        headers = {'content-type': 'application/json'}
-        auth_result = requests.get(url, headers=headers,)
-        auth_data = auth_result.json()
+        request.session['user_id'] = user_id 
+        request.session['user_name'] = user_name 
+    url = "%s%s%s%s%s%s" % (auth_url, "/api/grouprole/?uid=", auth_username, app_key, auth_key, app_name)
+    headers = {'content-type': 'application/json'}
+    auth_result = requests.get(url, headers=headers,)
+    auth_data = auth_result.json()
 
-        if 'admin' in auth_data['groups']:
-            request.session['user_role']='admin'
+    if 'admin' in auth_data['groups']:
+        request.session['user_role']='admin'
 
-        url = "%s%s%s%s%s" % (auth_url, "/api/allkey/?",app_key, auth_key, app_name)
-        headers = {'content-type': 'application/json'}
-        auth_result = requests.get(url, headers=headers,)
-        for item in auth_result.json():
-            if item['uid'] == auth_username :
-                request.session['user_key'] = item['key']
-        request.session.set_expiry(6000)
-    except IOError:
-	pass
+    url = "%s%s%s%s%s" % (auth_url, "/api/allkey/?",app_key, auth_key, app_name)
+    headers = {'content-type': 'application/json'}
+    auth_result = requests.get(url, headers=headers,)
+    for item in auth_result.json():
+        if item.get('uid') == auth_username :
+            request.session['user_key'] = item['key']
+    request.session.set_expiry(6000)
     return HttpResponseRedirect('/')
